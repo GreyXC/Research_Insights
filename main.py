eafc1fcd6d517e44b4456e4caa6c6eee6509825c
Matteo Bergstedt <your-email@example.com>
2025-10-11 22:18:58 +0100

    add: files

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("hello world");
+from scripts.extraction.extract_sources import load_local_csv
+from scripts.cleaning.clean_data import clean
+from scripts.analysis.analyze_insights import extract_keywords
+
+def run_pipeline():
+    df = load_local_csv("data_sources/raw/source.csv")
+    df_clean = clean(df)
+    keywords = extract_keywords(df_clean, "abstract")
+    print("Top Keywords:", keywords)
+
+if __name__ == "__main__":
+    run_pipeline()
